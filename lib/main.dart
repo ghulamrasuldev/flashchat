bb8b145dae56839ae63f26cd4e731a9f82d30753
ghulamrasuldev <ghulam.rasool.freelance@gmail.com>
2021-04-15 14:39:26 +0500

    Added Welcome Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
+import 'package:flash/WelcomeScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(FlashChat());
+
+}
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
 }

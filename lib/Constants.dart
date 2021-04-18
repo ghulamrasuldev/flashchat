5c15f54e0686d59c17773355e71b1c4cde4d6333
ghulamrasuldev <ghulam.rasool.freelance@gmail.com>
2021-04-18 12:43:30 +0500

    Added Basic Animations

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -5,13 +5,11 @@ const kSendButtonTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
   fontSize: 18.0,
 );
-
 const kMessageTextFieldDecoration = InputDecoration(
   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
   hintText: 'Type your message here...',
   border: InputBorder.none,
 );
-
 const kMessageContainerDecoration = BoxDecoration(
   border: Border(
     top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
